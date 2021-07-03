105adb2aadb64f9679c42ebe98647653cf8f1514
Joseph Willer <me@josephwiller.com>
2021-07-03 14:04:38 -0400

    importing spacy

--- court_docs/processor/views.py
+++ court_docs/processor/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from mini_clerk import extract_info as ei
 
 # Create your views here.
 
 def index(request):
+    print(ei.process("hello 2810"))
     return HttpResponse("Hello, world. You're at the polls index.")
--- court_docs/start_it_up.sh
+++ court_docs/start_it_up.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 
 
+
+echo "is this thing on"
+
 python -m spacy download en_core_web_sm
+
+python manage.py makemigrations
+python manage.py migrate
 
 python manage.py runserver 0.0.0.0:8000
